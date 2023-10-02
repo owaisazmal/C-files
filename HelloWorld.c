5f20eaeba7e395ff0f803a7cbdcc48da6bc6522b
Owais <owais.khan.244@my.csun.edu>
2023-10-01 21:24:17 -0700

    init

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,7 +2,23 @@
 
 int main(){
     //comment
-    printf("Hello World");
+    // printf("Hello World");
+    // printf("\n1\t2\t3\n4\t5\t6");
+
+    //return 0;
+
+    //Variables
+
+    int x;
+    x = 123;
+    int y = 456;
+
+    int age = 21;
+    float gpa = 2.05;
+    char grade = 'C';
+    char name[] = "Bro";
+
+    printf("You are %d years old",age);
     return 0;
-    
+
 }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,2 @@
+    printf("Hello World");
+    printf("\n1\t2\t3\n4\t5\t6");
