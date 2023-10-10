c5c0a91e1d133cf6bfa610edeae3a19348e250a1
Owais <owais.khan.244@my.csun.edu>
2023-10-09 20:53:56 -0700

    ternary

---
+++ ternaryOP.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int findMax(int x, int y){
+
+    // if (x > y)
+    // {
+    //     return x;
+    // }
+
+    // if (x < y)
+    // {
+    //     return y;
+    // }
+
+    return (x > y) ? x : y;
+
+}
+
+int main(){
+
+    int max = findMax(50, 10);
+    printf("%d", max);
+
+    return 0;
+
+}
+
