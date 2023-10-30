174f0b36e2feb63980ee9438d25eed93635482ea
Owais <owais.khan.244@my.csun.edu>
2023-10-29 17:26:57 -0700

    Complete game

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,7 +1,1 @@
-    // strlwr(string1); //coverts into lower case
-    // strupr(string1); //coverts into upper case
-    // strcat(string1, string2); //appends string2 to end of string1
-    // strncat(string1, string2, 1); //appends n character from string2 to string1
-    // strcpy(string1, string2); //copy string2 to string1
-    // strn(string1, string2, 1); //copy n characters of string to string1
-    // strrev(string1); //reverses the string
+res
